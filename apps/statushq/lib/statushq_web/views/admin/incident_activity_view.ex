1efea7a3bd4c48093fa5106cb108a53cc132787d
Eduardo Messuti <messuti.edd@gmail.com>
2017-12-04 15:15:20 +0100

    disabling notification features according to plan in UI

--- apps/statushq/lib/statushq_web/views/admin/incident_activity_view.ex
+++ apps/statushq/lib/statushq_web/views/admin/incident_activity_view.ex
@@ -9,4 +9,24 @@ defmodule StatushqWeb.Admin.IncidentActivityView do
   def render("subheader.html", assigns) do
     render(StatushqWeb.Admin.SharedView, "main_actions.html", assigns)
   end
+
+  def get_activity_types(conn) do
+    if conn.assigns.incident do
+      if conn.assigns.incident.type != "s" do
+        Enum.filter(conn.assigns.activity_types, fn {k, _id} ->
+          k != "Retroactive" && k != "Scheduled"
+        end)
+      else
+        Enum.filter(conn.assigns.activity_types, fn {k, _id} -> k == "Resolved" end)
+      end
+    else
+      conn.assigns.activity_types
+    end
+  end
+
+  def get_activity_default_value(conn) do
+    if !conn.assigns.incident && conn.params["maintenance"] === "true" do
+      StatushqWeb.Admin.IncidentView.maintenance_type(conn).id
+    end
+  end
 end
--- apps/statushq/lib/with_pro.ex
+++ apps/statushq/lib/with_pro.ex
@@ -1,6 +1,10 @@
 defmodule WithPro do
   def pro?(), do: System.get_env("PRO") && Code.ensure_compiled?(StatushqPro)
 
+  def disabled_for_plan(conn, plan, do: block) do
+    if !pro?() || conn.assigns.status_page.plan != plan, do: block
+  end
+
   defmacro with_pro(do: block) do
     if pro? do
       IO.puts "****** PRO ON"
