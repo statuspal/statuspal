221bdde6412075a94bce0865a4626160c750f7be
Eduardo Messuti <messuti.edd@gmail.com>
2017-12-04 12:41:56 +0100

    version bump

--- apps/statushq/mix.exs
+++ apps/statushq/mix.exs
@@ -3,7 +3,7 @@ defmodule StatushqWeb.Mixfile do
 
   def project do
     [app: :statushq,
-     version: "1.0.5",
+     version: "1.0.6",
      elixir: "~> 1.5.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
