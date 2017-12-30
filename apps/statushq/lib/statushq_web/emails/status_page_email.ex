f32d13427723aa6fe22508a6b4ad82ac238fce39
Eduardo Messuti <messuti.edd@gmail.com>
2017-12-30 10:59:17 +0100

    fix: send notification emails to subscribed users

--- apps/statushq/lib/statushq_web/emails/status_page_email.ex
+++ apps/statushq/lib/statushq_web/emails/status_page_email.ex
@@ -3,11 +3,12 @@ Code.ensure_loaded Phoenix.Swoosh
 defmodule StatushqWeb.StatusPageEmail do
   use Phoenix.Swoosh, view: StatushqWeb.EmailView, layout: {StatushqWeb.LayoutView, :email}
   alias Swoosh.Email
+  alias StatushqWeb.MailGun
 
   def status_notification(page, activity, incident) do
     %Email{}
     |> from(Coherence.Config.email_from)
-    |> to("messuti.edd@gmail.com")
+    |> to("page-#{page.id}@#{MailGun.domain()}")
     |> subject("[#{activity.activity_type.name}] #{String.capitalize(page.name)} Status Update")
     |> render_body("status_notification.html", %{page_name: page.name, activity: activity, incident: incident})
   end
