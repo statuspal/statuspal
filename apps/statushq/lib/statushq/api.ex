4728a07da54c704196d7d1747c47f533bc793a34
Eduardo Messuti <messuti.edd@gmail.com>
2018-01-12 23:07:52 +0100

    make monitoring status calls instead of casts

--- apps/statushq/lib/statushq/api.ex
+++ apps/statushq/lib/statushq/api.ex
@@ -8,16 +8,16 @@ defmodule Statushq.Api do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def handle_cast([:status_change, subscriber_ids, new_status], state) do
+  def handle_call([:status_change, subscriber_ids, new_status], _from, state) do
     Logger.debug ":status_change #{inspect(subscriber_ids)} to #{new_status}"
     subscriber_ids
     |> Enum.map(&String.to_integer/1)
     |> SPM.set_services_up(new_status == "up")
-    {:noreply, state}
+    {:reply, :ok, state}
   end
 
   # GenServer.cast({Statushq.Api, :"statushq@127.0.0.1"}, [:notify_status, ["17", "23"], "down"])
-  def handle_cast([:notify_status, subscriber_ids, status], state) do
+  def handle_call([:notify_status, subscriber_ids, status], _from, state) do
     Logger.debug ":notify_status #{inspect(subscriber_ids)} to #{status}"
 
     subscriber_ids
@@ -28,7 +28,7 @@ defmodule Statushq.Api do
     |> StatusPageEmail.service_status_notification(status)
     |> Mailer.deliver
 
-    {:noreply, state}
+    {:reply, :ok, state}
   end
 
   def get_vars(service) do
