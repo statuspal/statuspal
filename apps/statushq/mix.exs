945e36056ee40f07901c23dfb58a800057a57d2b
Eduardo Messuti <messuti.edd@gmail.com>
2018-01-27 11:38:19 +0100

    version 1.4.2

--- apps/statushq/mix.exs
+++ apps/statushq/mix.exs
@@ -3,7 +3,7 @@ defmodule StatushqWeb.Mixfile do
 
   def project do
     [app: :statushq,
-     version: "1.4.1",
+     version: "1.4.2",
      elixir: "~> 1.5.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
--- apps/statushq/version.exs
+++
@@ -1,3 +0,0 @@
-defmodule Statushq.Version do
-  def get(), do: "1.4.1"
-end
