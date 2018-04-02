4ac9be2cf177dc6f24c855a94637cf7eabe3e8a1
Eduardo Messuti <messuti.edd@gmail.com>
2018-04-02 17:56:03 +0200

    made google analytics based on an env variable

--- apps/statushq/lib/statushq_web/views/shared_view.ex
+++ apps/statushq/lib/statushq_web/views/shared_view.ex
@@ -2,4 +2,6 @@ defmodule StatushqWeb.SharedView do
   use StatushqWeb, :view
   import WithPro
   with_pro do: use StatushqProWeb.SharedView
+
+  def ga_code(), do: Application.get_env(:statushq, :google_analytics_id)
 end
