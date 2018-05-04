db3a6a8d22dc2080c3716eace7f46c7facefb808
Eduardo Messuti <messuti.edd@gmail.com>
2018-05-04 17:12:11 +0200

    version 1.9.4

--- apps/statushq/mix.exs
+++ apps/statushq/mix.exs
@@ -3,8 +3,7 @@ defmodule StatushqWeb.Mixfile do
 
   def project do
     [app: :statushq,
-     version: "1.9.3",
-     elixir: "~> 1.5.1",
+     version: "1.9.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -13,7 +12,8 @@ defmodule StatushqWeb.Mixfile do
      deps: deps(),
      build_path: "../../_build",
      lockfile: "../../mix.lock",
-     deps_path: "../../deps"]
+     deps_path: "../../deps",
+     elixir: "~> 1.5.1"]
   end
 
   def pro(), do: System.get_env("PRO")
